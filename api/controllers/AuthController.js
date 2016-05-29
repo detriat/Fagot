5633bd401edab2e101b4da8abf88c4e240b8c309
Дмитрий <detriat@ukr.net>
2016-05-29 19:15:30 +0300

    AuthController

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -10,7 +10,10 @@ module.exports = {
       Vk.token(code, function(err, item){
         sails.log(item);
         Vk.getuser(item['access_token'], item['user_id'], function(err, item){
-            res.send(200, item);
+          sails.log(item.response[0].first_name);
+          var url = "http://localhost:8100/#/app/start?name="+item.response[0].first_name;
+          return res.redirect(url);
+
         })
       });
     }
